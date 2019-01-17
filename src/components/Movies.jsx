d6f22dad628ea427eae6338f7b8a999d6579ce94
React <j.aquarone@gmail.com>
2019-01-17 15:01:24 -0500

    handling page changes

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Pagination from './Common/Pagination';
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
-  }
+    movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4
+  };
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter(movies => movie._id !== movies._id);
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
   }
 
   render() {
 
     const { length: count } = this.state.movies;
+    const { pageSize , currentPage } = this.state;
 
     if(count === 0)
       return <p>There are no movies in the database</p>
@@ -51,6 +59,11 @@ class Movies extends Component {
         ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange} />
       </React.Fragment>
     )
   }
