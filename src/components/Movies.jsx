a4535c7019c463f2d8b0a4a4511feab6c9c50dbc
React <j.aquarone@gmail.com>
2019-01-17 01:16:08 -0500

    added a delete btn in Movies component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -15,6 +15,7 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +24,7 @@ class Movies extends Component {
             <td>{movie.title}</td>
             <td>{movie.genre.name}</td>
             <td>{movie.dailyRentalRate}</td>
+            <td><button className="btn btn-danger btn-sm">Delete</button></td>
           </tr>
       ))}
 
