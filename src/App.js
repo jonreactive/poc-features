effdf461bc72bb1dfd9a8421ec16bca83c8348ec
React <j.aquarone@gmail.com>
2019-01-17 00:54:23 -0500

    bootstrap + fake database + Movie component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Movies from './components/Movies';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World Git Hub 1st Commit</h1>
-      </div>
+      <main className="container">
+        <h1>test bootstrap</h1>
+        <Movies />
+      </main>
     );
   }
 }
---
+++ src/components/Movies.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {
+  state = {
+
+  }
+
+  render() {
+    return ( <h2>hello from movies component</h2> )
+  }
+
+}
+
+export default Movies
